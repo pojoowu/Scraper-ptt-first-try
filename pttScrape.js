372ba6683b8cf46fa020d07a52c9c70d99a8fc60
pojoowu <82290440+pojoowu@users.noreply.github.com>
2021-04-28 19:03:31 +0200

    Using import/export method

--- pttScrape.js
+++ pttScrape.js
@@ -1,13 +1,8 @@
-function pttTitles(section, callback) {
-  const request = require('request');
-  const cheerio = require('cheerio');
-  const fs = require('fs');
-  const writeStream = fs.createWriteStream('post.csv', {
-    encoding: 'utf8'
-  });
-
-  writeStream.write(`Title, Content, Link, Comments, Date \n`);
-
+// const request = require('request');
+// const cheerio = require('cheerio');
+import request from 'request';
+import cheerio from 'cheerio';
+export default function (section, callback) {
   let database = [],
     index = 0;
   //for (let i = 0; i < 5; i++) {
@@ -16,6 +11,7 @@ function pttTitles(section, callback) {
       const $ = cheerio.load(html);
 
       $('.r-ent').each((i, el) => {
+        //grabbing the title, link, date
         const title = $(el).find('.title a').text();
         const link = $(el).find('a').attr('href');
         const date = $(el).find('.meta .date').text();
@@ -26,18 +22,18 @@ function pttTitles(section, callback) {
           content: '',
           comments: 0
         };
+        //grabbing contents in each page
         request(`http://www.ptt.cc${link}`, (error, response, html) => {
           if (!error && response.statusCode === 200) {
+
             let $ = cheerio.load(html);
-            let count = 0;
             $('.push').each(() => {
               data.comments++;
             });
-            data.content = $('#main-content').children().remove().end().text().replace(/\n|\t/g, ' ');
+            data.content = $('#main-content').children().remove().end().text().replace(/\n|,/g, ' ').replace(/\t/g, ' ');
             callback(data);
           }
         });
-
       })
     }
     console.log("Done writing");
--- scrape.js
+++ scrape.js
@@ -1,54 +1,13 @@
 let database = [],
   index = 0;
-const fs = require('fs');
+import fs from 'fs';
 let writeStream = fs.createWriteStream('post.csv');
+import fn from './pttScrape.js';
 
 //Write Headers
 writeStream.write(`Title, Link, Content, NbComments, Date \n`);
 
-pttTitles("Hearthstone", (data) => {
+fn("Hearthstone", (data) => {
   writeStream.write(`${data.title}, ${data.link}, ${data.content},
     ${data.comments}, ${data.date} \n`);
 });
-
-function pttTitles(section, callback) {
-  const request = require('request');
-  const cheerio = require('cheerio');
-  let database = [],
-    index = 0;
-  //for (let i = 0; i < 5; i++) {
-  request(`https://www.ptt.cc/bbs/${section}/index.html`, (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-      const $ = cheerio.load(html);
-
-      $('.r-ent').each((i, el) => {
-        //grabbing the title, link, date
-        const title = $(el).find('.title a').text();
-        const link = $(el).find('a').attr('href');
-        const date = $(el).find('.meta .date').text();
-        let data = {
-          title: title,
-          link: link,
-          date: date,
-          content: '',
-          comments: 0
-        };
-        //grabbing contents in each page
-        request(`http://www.ptt.cc${link}`, (error, response, html) => {
-          if (!error && response.statusCode === 200) {
-
-            let $ = cheerio.load(html);
-            $('.push').each(() => {
-              data.comments++;
-            });
-            data.content = $('#main-content').children()
-              .remove().end().text().replace(/\n|\t/g, ' ');
-            console.log(data);
-            callback(data);
-          }
-        });
-      })
-    }
-    console.log("Done writing");
-  });
-}
