f9bb8f06b69c2bbe4ca0667515d1a2ca9962a19c
pojoowu <82290440+pojoowu@users.noreply.github.com>
2021-04-28 17:14:01 +0200

    Scrape with content
    Change so that it saves the content in a csv file.

--- scrape.js
+++ scrape.js
@@ -1,30 +1,44 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv', {
+  encoding: 'utf8'
+});
+
+writeStream.write(`Title, Content, Link, Comments, Date \n`);
+
 let database = [],
   index = 0;
-for (let i = 0; i < 5; i++) {
-  request(`https://www.ptt.cc/bbs/Hearthstone/index${2716-index}.html`, (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-      const $ = cheerio.load(html);
+//for (let i = 0; i < 5; i++) {
+request(`https://www.ptt.cc/bbs/Hearthstone/index.html`, (error, response, html) => {
+  if (!error && response.statusCode === 200) {
+    const $ = cheerio.load(html);
 
-      $('.r-ent').each((i, el) => {
-        const title = $(el).find('.title a').text();
-        const link = $(el).find('a').attr('href');
-        request(link, (errorIn, responseIn, htmlIn) => {
-          if(!errorIn && responseIn.statusCode === 200){
-            let mm = cheerio.load(htmlIn);
+    $('.r-ent').each((i, el) => {
+      const title = $(el).find('.title a').text();
+      const link = $(el).find('a').attr('href');
+      const date = $(el).find('.meta .date').text();
+      let data = {
+        title: title,
+        link: link,
+        date: date,
+      };
+      request(`http://www.ptt.cc${link}`, (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+          let $ = cheerio.load(html);
+          let count = 0;
+          $('.push').each(() => {
+            count++;
+          });
+          const content = $('#main-content').children()
+            .remove().end().text().replace(/\n|\t/g, `\s`);
+          writeStream.write(`${title}, ${link}, ${content}, ${count}, ${date} \n`);
+        }
+      });
 
-          }
-        });
-        const date = $(el).find('.meta').children('.date').text();
-        let data = {
-          title: title,
-          link: link,
-          date: date
-        };
-        database.push(data);
-      })
-    }
-  });
-  index++;
-}
+    })
+  }
+  console.log("Done writing");
+});
+//  index++;
+//}
