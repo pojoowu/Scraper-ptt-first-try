aa173050661aa493e2d2816863d074a2eb99e005
pojoowu <paulpaul950052@gmail.com>
2021-04-29 21:07:32 +0200

    Final Update

--- pttScrape.js
+++ pttScrape.js
@@ -2,40 +2,78 @@
 // const cheerio = require('cheerio');
 import request from 'request';
 import cheerio from 'cheerio';
-export default function (section, callback) {
-  let database = [],
-    index = 0;
-  //for (let i = 0; i < 5; i++) {
-  request(`https://www.ptt.cc/bbs/${section}/index.html`, (error, response, html) => {
-    if (!error && response.statusCode === 200) {
-      const $ = cheerio.load(html);
+export default async function grabPttArticles(section, startingPage, k) {
+  const indices = Array(Math.floor(k/10)).fill(0).map((val, index) => startingPage - index);
+  const database = await Promise.all(indices.map((index) => getPttArticleByIndex(section, index)));
+  return [].concat(...(database)).splice(0, k);
+}
 
-      $('.r-ent').each((i, el) => {
-        //grabbing the title, link, date
-        const title = $(el).find('.title a').text();
-        const link = $(el).find('a').attr('href');
-        const date = $(el).find('.meta .date').text();
-        let data = {
-          title: title,
-          link: link,
-          date: date,
-          content: '',
-          comments: 0
-        };
-        //grabbing contents in each page
-        request(`http://www.ptt.cc${link}`, (error, response, html) => {
-          if (!error && response.statusCode === 200) {
+async function getPttArticleByIndex(section, index){
+  const html = await findHTML(`https://www.ptt.cc/bbs/${section}/index${index}.html`);
+  const $body = cheerio.load(html);
+  const $titles = $body('.r-ent');
+  const results = await Promise.all($titles.map((i, el) => grabData($body(el))));
+  return results.reverse();
+}
+//   let j = 0,
+//     stopping = false;
+//   while (j < k && !stopping) {
+//     let html = await findHTML(`${section}/index${startingPage - j}`);
+//     let $ = cheerio.load(html);
+//     $('.r-ent').each(async function (i, el) {
+//       let data = await grabData(i, el, $);
+//       if (database.length < k) {
+//         database.push(data);
+//       } else {
+//         stopping = true;
+//       }
+//     });
+//     j++;
+//   }
+//   console.log("Done");
+//   return database;
+// }
 
-            let $ = cheerio.load(html);
-            $('.push').each(() => {
-              data.comments++;
-            });
-            data.content = $('#main-content').children().remove().end().text().replace(/\n|,/g, ' ').replace(/\t/g, ' ');
-            callback(data);
+async function grabData($el) {
+  //grabbing the title, link, date
+  const title = $el.find('.title a').text();
+  const link = $el.find('.title a').attr('href');
+  const date = $el.find('.meta .date').text();
+  const data = {
+    title: title,
+    link: link,
+    date: date,
+    content: null,
+    comments: null
+  };
+  try {
+    const html = await findHTML(`http://www.ptt.cc/${link}`);
+    const $ = cheerio.load(html);
+    data.commemts = $('.push').length;
+    data.content = $('#main-content').find('.push, .richcontent, .f2')
+      .remove().end().text().replace(/\n|,|\t/g, ' ');
+  } catch (error) {
+    data.content = "deleted";
+  }
+  return data;
+}
+
+
+function findHTML(link) {
+  return (
+    new Promise((resolve, reject) => {
+      request(link,
+        (error, response, html) => {
+          if (error) {
+            reject(error);
+          } 
+          else if(response.statusCode !== 200){
+            reject(new Error(`Get response with status code ${response.statusCode}`));
+          }
+          else {
+            resolve(html);
           }
         });
-      })
-    }
-    console.log("Done writing");
-  });
+    })
+  );
 }
--- scrape.js
+++ scrape.js
@@ -1,13 +1,19 @@
-let database = [],
-  index = 0;
 import fs from 'fs';
 let writeStream = fs.createWriteStream('post.csv');
 import fn from './pttScrape.js';
 
 //Write Headers
 writeStream.write(`Title, Link, Content, NbComments, Date \n`);
+async function main() {
+  const database = await fn("Hearthstone", 2716, 100);
+  try {
+    for (let data of database) {
+      writeStream.write(`${data.title}, ${data.link}, ${data.content},
+  ${data.comments}, ${data.date} \n`);
+    }
+  } catch (error) {
 
-fn("Hearthstone", (data) => {
-  writeStream.write(`${data.title}, ${data.link}, ${data.content},
-    ${data.comments}, ${data.date} \n`);
-});
+  }
+}
+
+main();
